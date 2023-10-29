09fb8898a636f1aa1f8eb1ed05b1a91c113708da
Swanchick <61909965+Swanchick@users.noreply.github.com>
2023-10-29 13:20:25 +0200

    New model / i'm done :D

--- lua/gmod_lock/lock1/cl.lua
+++ lua/gmod_lock/lock1/cl.lua
@@ -1,62 +1,135 @@
+
+
+local backgroundModel = Color(0, 0, 0, 200)
+local lockModel = ClientsideModel("models/door_handle.mdl")
+local lockModelPos = Vector(0, -35, 20)
+local lockModelAngle = Angle(0, 90, 0)
+
+local lockPickModel= ClientsideModel("models/lockpick.mdl")
+
 do
     local PANEL = {}
 
     function PANEL:Init()
         self:MakePopup()
 
-        self.Buttons = {}
+        self.RotateGoal = {1000, 1000}
 
-        for i=1, 10 do
-            local button = vgui.Create("DButton", self)
-            if not IsValid(button) then continue end
-            table.insert(self.Buttons, button)
-
-            button:SetText(tostring(i))
-            button:Dock(RIGHT)
-
-            function button:DoClick()
-                local parent = self:GetParent()
-                if not IsValid(parent) then return end
-                
-                local lockName = parent.LockName
-                LOCKSYSTEM:CheckResult(lockName, {self:GetText()})
-            end
-        end
+        self.PreviousAngle = 0
+        self.CanPlaySound = true
     end
 
-    function PANEL:SetLockName(lockName)
+    function PANEL:PerformLayout()
+        local scrW, scrH = ScrW(), ScrH()
+
+        self:SetSize(scrW, scrH)
+    end
+
+    function PANEL:Setup(lockName, data)
         self.LockName = lockName
+        local angle = data[1]
+        local offset = math.pi / 80
+
+        self.RotateGoal = {angle - offset, angle + offset}
     end
 
-    function PANEL:PerformLayout(w, h)
-        local wide, tall = ScreenScale(230), ScreenScale(20)
+    function PANEL:GetAngle(sX, sY, eX, eY)
+        local dx = eX - sX
+        local dy = eY - sY
+        local angle = math.atan2(dy, dx)
+        return math.Clamp(angle, -math.pi + 0.001, 0.001)
+    end
+
+    function PANEL:GetAngleSpeed(sX, sY, eX, eY)
+        local delta = FrameTime()
+        local angle = self:GetAngle(sX, sY, eX, eY)
         
-        self:SetSize(wide, tall)
-        self:Center()
+        local speed = (angle - self.PreviousAngle) / delta
+
+        self.PreviousAngle = angle
+        
+        return speed
     end
 
-    vgui.Register("Lock.SuperHardLock", PANEL, "EditablePanel")
+    function PANEL:OnMousePressed(keyPressed)
+        if keyPressed ~= MOUSE_LEFT then return end
+        
+        local scrW, scrH = ScrW(), ScrH()
+        local mouseX, mouseY = self:LocalCursorPos()
+
+        if not self.CanPlaySound then return end
+        local speedAngle = self:GetAngleSpeed(scrW / 2, scrH / 2, mouseX, mouseY)
+        if math.abs(speedAngle) > 0.5 then return end
+        
+        self.Angle = self:GetAngle(scrW / 2, scrH / 2, mouseX, mouseY)
+
+        LOCKSYSTEM:CheckResult(self.LockName, {self.Angle})
+        
+        surface.PlaySound("buttons/combine_button7.wav")
+    end
+
+    function PANEL:Think()
+        local scrW, scrH = ScrW(), ScrH()
+        local mouseX, mouseY = self:LocalCursorPos()
+
+        self.Angle = self:GetAngle(scrW / 2, scrH / 2, mouseX, mouseY)
+        if self.Angle < self.RotateGoal[1] or self.Angle > self.RotateGoal[2] then return end
+        
+        if not self.CanPlaySound then return end
+        local speedAngle = self:GetAngleSpeed(scrW / 2, scrH / 2, mouseX, mouseY)
+        
+        if math.abs(speedAngle) > 0.5 or math.abs(speedAngle) < 0.001 then return end
+
+        self.CanPlaySound = false
+        
+        timer.Simple(1, function ()
+            self.CanPlaySound = true
+        end)
+
+        surface.PlaySound("buttons/combine_button7.wav")
+    end
+
+    function PANEL:Paint(w, h)
+        local eyeAngle = EyeAngles()
+        local pos = eyeAngle:Forward() * lockModelPos.y + eyeAngle:Up() * lockModelPos.z
+    
+        surface.SetDrawColor(backgroundModel)
+        surface.DrawRect(0, 0, w, h)
+        
+        local mouseX, mouseY = self:LocalCursorPos()
+        local lockAng = Angle(0, eyeAngle.y - lockModelAngle.y, -eyeAngle.x)
+        local lockPickAng = Angle(0, eyeAngle.y, eyeAngle.z + math.deg(self.Angle) - 180)
+
+        cam.Start3D(pos)
+            lockModel:SetAngles(lockAng)
+            lockModel:DrawModel()
+
+            lockPickModel:SetPos(Vector(-10, 0, 20.5))
+            lockPickModel:SetAngles(lockPickAng)
+            lockPickModel:DrawModel()
+        cam.End3D()
+    end
+
+    vgui.Register("Lock.Lock1", PANEL, "EditablePanel")
 end
 
-TEST_LOCK = TEST_LOCK or {}
-
-if IsValid(TEST_LOCK) then
-    TEST_LOCK:Remove()
+if IsValid(LOCK1) then
+    LOCK1:Remove()
 end
 
 local function OnActivate(lockName, data)
-    if IsValid(TEST_LOCK) then return end
-    
-    TEST_LOCK = vgui.Create("Lock.SuperHardLock")
-    TEST_LOCK:SetLockName(lockName)
+    if IsValid(LOCK1) then return end 
+
+    LOCK1 = vgui.Create("Lock.Lock1", GetHUDPanel())
+    LOCK1:Setup(lockName, data)
 end
 
 local function OnSuccess()
-    TEST_LOCK:Remove()
+    LOCK1:Remove()
 end
 
 local function OnFail()
-    -- TEST_LOCK:Remove()
+    LOCK1:Remove()
 end
 
 LOCKSYSTEM:RegisterLock("lock_1", OnActivate, OnSuccess, OnFail)
--- lua/gmod_lock/lock1/sv.lua
+++ lua/gmod_lock/lock1/sv.lua
@@ -1,7 +1,10 @@
 local function OnActivate(ply)
-    local rightAnswer = math.random(1, 10)
+    local angle = math.Rand(0, -math.pi)
+    local offset = math.pi / 80
+    local min = angle - offset
+    local max = angle + offset
     
-    return {rightAnswer}, {}
+    return {min, max}, {angle}
 end
 
 local function OnSucces(ply, door)
@@ -17,7 +20,7 @@ local function CheckResults(result, data)
     local numberResult = result[1]
     local rightAnswer = data[1]
 
-    return tonumber(numberResult) == tonumber(rightAnswer)
+    return numberResult >= data[1] and numberResult <= data[2]
 end
 
 LOCKSYSTEM:RegisterLock("lock_1", OnActivate, OnSucces, OnFail, CheckResults)
